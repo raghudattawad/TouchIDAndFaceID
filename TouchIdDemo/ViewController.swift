c67c7288f342f4a5c1c670adbacdd6f3450201a6
Raghavendra <raghudattawad>
2019-09-22 11:04:19 +0530

    Demo with Touch id and Face id

---
+++ TouchIdDemo/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  TouchIdDemo
+//
+//  Created by Raghavendra on 22/09/19.
+//  Copyright © 2019 To Be Success echnology. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+  
+    beginFaceID()
+    }
+
+    @IBAction func Authenticate(_ sender: Any) {
+    
+    
+        let context = LAContext()
+        var error: NSError?
+        
+        // 2
+        // check if Touch ID is available
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 3
+            let reason = "Authenticate with Touch ID"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
+                {(success, error) in
+                    // 4
+                    if success {
+                        self.showAlertController("Touch ID Authentication Succeeded")
+                    }
+                    else {
+                        self.showAlertController("Touch ID Authentication Failed")
+                    }
+            })
+        }
+            // 5
+        else {
+            showAlertController("Touch ID not available")
+        }
+    
+    }
+    
+    func showAlertController(_ message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func beginFaceID() {
+        
+        guard #available(iOS 8.0, *) else {
+            return print("Not supported")
+        }
+        
+        let context = LAContext()
+        var error: NSError?
+        
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return print(error)
+        }
+        
+        let reason = "Face ID authentication"
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { isAuthorized, error in
+            guard isAuthorized == true else {
+                return print(error)
+            }
+            
+            print("success")
+        }
+}
+}
+
